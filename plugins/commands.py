0be6c88632cd9ed338ec43b1c122ea1250e060ca
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2021-05-25 06:10:53 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,9 +22,9 @@ from pyrogram import Client, filters
 
 
 
-HOME_TEXT = "👋🏻 **Hi [{}](tg://user?id={})**,\n\nI'm **Radio Player Bot** \nI Can Play Radio/Stream Music In Channels & Groups 24x7 Nonstop. Made with ❤️ By @AsmSafone!"
+HOME_TEXT = "👋🏻 **Bismillah [{}](tg://user?id={})**,\n\nI'm **Radio Player Bot** \nI Can Play Radio/Stream Mawaidha In Channels & Groups 24x7 Nonstop. Made with ❤️ By @Twuwbaa!"
 HELP = """🏷️ **Need Help?** 🤔
-__(Join @SafoTheBot For Support)__
+__(Join @HudumaChat For Support)__
 
 🏷️ **Common Commands**:
 \u2022 `/play` reply to an audio to play or queue it
@@ -57,8 +57,8 @@ __(Join @SafoTheBot For Support)__
 async def start(client, message):
     buttons = [
         [
-        InlineKeyboardButton('CHANNEL', url='https://t.me/AsmSafone'),
-        InlineKeyboardButton('SUPPORT', url='https://t.me/SafoTheBot'),
+        InlineKeyboardButton('CHANNEL', url='https://t.me/Mawaidha1'),
+        InlineKeyboardButton('SUPPORT', url='https://t.me/viongiziBot'),
     ],
     [
         InlineKeyboardButton('MORE BOTS', url='https://t.me/AsmSafone/12'),
